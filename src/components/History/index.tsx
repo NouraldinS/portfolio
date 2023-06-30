d1f942dee813458900acce4039d41c5819e85625
Noureldean <noureldean.saed@dash.ps>
2023-06-30 23:17:10 +0300

    Fixed build issue

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -1,43 +1,32 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Timeline } from 'antd';
 import { MY_HISTORY } from './data';
 
-const History = () => {
-  useEffect(() => {
-    const onScroll = (event: Event) => {
-      console.log({ event });
-    };
-    document.addEventListener('scroll', onScroll);
-    return () => {
-      document.removeEventListener('scroll', onScroll);
-    };
-  }, []);
-  return (
-    <div id="work" className="relative w-1/2 mx-auto pt-52 snap-start scroll-mt-5">
-      <div className="text-cold-200 text-xl mb-4">My Experiences</div>
-      <Timeline
-        mode="left"
-        items={MY_HISTORY.map(({
-          date, title, company, description,
-        }) => ({
-          label: <span className="text-xl text-cold-400">{date}</span>,
-          children: (
-            <div>
-              <div
-                className="absolute -left-[25vw] w-6 h-6 bg-hot-600 border-l-hot-400 animate-spin duration-10000"
-              />
-              <div className="text-xl text-cold-200">
-                {title}
-                {' - '}
-                {company}
-              </div>
-              <div className="text-lg text-cold-200">{description}</div>
+const History = () => (
+  <div id="work" className="relative w-1/2 mx-auto pt-52 snap-start scroll-mt-5">
+    <div className="text-cold-200 text-xl mb-4">My Experiences</div>
+    <Timeline
+      mode="left"
+      items={MY_HISTORY.map(({
+        date, title, company, description,
+      }) => ({
+        label: <span className="text-xl text-cold-400">{date}</span>,
+        children: (
+          <div>
+            <div
+              className="absolute -left-[25vw] w-6 h-6 bg-hot-600 border-l-hot-400 animate-spin duration-10000"
+            />
+            <div className="text-xl text-cold-200">
+              {title}
+              {' - '}
+              {company}
             </div>
-          ),
-        }))}
-      />
-    </div>
-  );
-};
+            <div className="text-lg text-cold-200">{description}</div>
+          </div>
+        ),
+      }))}
+    />
+  </div>
+);
 
 export default History;
